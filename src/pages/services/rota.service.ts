c3411cd9675cbf2422420f273f0713f8bde81943
Matheus Malinovski <matheus.malinovski@gmail.com>
2017-12-12 16:51:19 -0200

    probrema

--- src/pages/services/rota.service.ts
+++ src/pages/services/rota.service.ts
@@ -31,7 +31,7 @@ export class RotaService {
     return this.afDataBase.list(`/Clientes/${this.key}/Rotas`,{
       query: {
       orderByChild: 'iniciada',
-      equalTo: 'true'
+      equalTo:true
       }
       })
   }
