7a5fdd5afb696e547a227ad9b35972aafb11bae1
Matheus Malinovski <matheus.malinovski@gmail.com>
2017-12-13 08:10:51 -0200

    arruma rota (1/6)

--- src/pages/classes/Rota.ts
+++ src/pages/classes/Rota.ts
@@ -3,5 +3,6 @@ export class Rota{
         constructor(public descricao: string = "",
                     public horaInicio: string ="",
                     public horaTermino: string = "",                  
-                    public iniciada: boolean = false){}
+                    public iniciada: boolean = false,
+                    public motoristaKeyTemp: string = ""){}
     }
--- src/pages/rotas/rotas.ts
+++ src/pages/rotas/rotas.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { FirebaseListObservable } from 'angularFire2/database';
 import { RotaService } from '../services/rota.service';
 import { UsuarioService } from '../services/usuario.service';
 import { ItinerarioMotoristaPage } from '../itinerario-motorista/itinerario-motorista';
+import { Rota } from '../classes/Rota';
 
 /**
  * Generated class for the RotasPage page.
@@ -22,7 +23,8 @@ export class RotasPage {
   listaRotas: FirebaseListObservable<any>;
   listaRotasIniciadas: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public _serviceRota: RotaService, public _serviceUsuario: UsuarioService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public _serviceRota: RotaService,
+             public _serviceUsuario: UsuarioService, public toastCtrl: ToastController) {
     this._serviceRota.key = this._serviceUsuario.acesso.clienteKey;
 
     this.listaRotas = this._serviceRota.lista();
@@ -33,8 +35,46 @@ export class RotasPage {
     this.navCtrl.push(ItinerarioMotoristaPage, {rotaKey: rota.$key});
   }
 
+  presentToast(msg: string = "OK") {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
+
   iniciarRota(rota: any){
-    this.navCtrl.push(ItinerarioMotoristaPage, {rotaKey: rota.$key});
+    let rA = new Rota();
+    
+    rA.descricao = rota.descricao;
+    rA.horaInicio = rota.horaInicio;
+    rA.horaTermino = rota.horaTermino;
+    rA.iniciada = true;
+    rA.motoristaKeyTemp = this._serviceUsuario.acesso.usuarioKey;
+
+    this._serviceRota.alterar(rota.$key, rA).then(() => {
+      this.presentToast("Rota Iniciada!");
+      this.navCtrl.push(ItinerarioMotoristaPage, {rotaKey: rota.$key});
+    }).catch(err =>{
+      this.presentToast(err.message);
+    })   
+  }
+
+  finalizarRota(rota: any){
+    let rA = new Rota();
+    
+    rA.descricao = rota.descricao;
+    rA.horaInicio = rota.horaInicio;
+    rA.horaTermino = rota.horaTermino;
+    rA.iniciada = false;
+    rA.motoristaKeyTemp = "";
+
+    this._serviceRota.alterar(rota.$key, rA).then(() => {
+      this.presentToast("Rota Finalizada!");
+      this.navCtrl.push(ItinerarioMotoristaPage, {rotaKey: rota.$key});
+    }).catch(err =>{
+      this.presentToast(err.message);
+    })   
   }
 
 }
